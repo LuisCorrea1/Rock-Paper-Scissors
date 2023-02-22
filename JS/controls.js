ce9cdb3134ff585e8218ab32ad4a424bc6a3a12d
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-02-21 21:17:29 -0600

    Function for selecting the computer choice

--- JS/controls.js
+++ JS/controls.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    let random = Math.floor(Math.random() * choices.length);
+    const result = choices[random];
+    console.log(result);
+}
+
+getComputerChoice();
